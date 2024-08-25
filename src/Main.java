4aefb6a65d70c9aa2cba166875871f06fda1e191
Diogo Tallys <dtma@ic.ufal.br>
2024-08-25 15:25:42 -0300

    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,14 @@
-import br.ufal.ic.p2.myfood.Facade;
-import easyaccept.EasyAcceptFacade;
-
-import java.util.ArrayList;
-import java.util.List;
+import easyaccept.EasyAccept;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> tests = new ArrayList<>();
-
-        tests.add("tests/us1_1.txt");
-        tests.add("tests/us1_2.txt");
-        tests.add("tests/us2_2.txt");
-        tests.add("tests/us2_2.txt");
-        tests.add("tests/us3_1.txt");
-        tests.add("tests/us3_2.txt");
-        tests.add("tests/us4_1.txt");
-        tests.add("tests/us4_2.txt");
-
-        EasyAcceptFacade eaFacade = new EasyAcceptFacade(new Facade(), tests);
-
-        // Execute the tests
-        eaFacade.executeTests();
-
-        // Print the tests execution results
-        System.out.println(eaFacade.getCompleteResults());
+        String[] args2 = {
+                "br.ufal.ic.p2.jackut.Facade",
+                "tests/us1_1.txt", "tests/us1_2.txt",
+                "tests/us2_1.txt", "tests/us2_2.txt",
+                "tests/us3_1.txt", "tests/us3_2.txt",
+                "tests/us4_1.txt", "tests/us4_2.txt",
+        };
+        EasyAccept.main(args2);
     }
 }
--- src/MyFood/Facade.java
+++ src/MyFood/Facade.java
@@ -1,34 +1,7 @@
-package MyFood;
-import java.util.UUID;
+package br.ufal.ic.p2.jackut;
+
+import easyaccept.EasyAccept;
 
 public class Facade {
-    private final MyFoodSystem system;
 
-    public Facade() {
-        system = new MyFoodSystem();
-    }
-
-    public void zerarSistema() {
-        system.zerarSistema();
-    }
-
-    public String getAtributoUsuario(String id, String nomeAtributo) {
-        return system.getAtributoUsuario(id, nomeAtributo);
-    }
-
-    public void criarUsuario(String nome, String email, String senha, String endereco) {
-        system.criarUsuario(nome, email, senha, endereco);
-    }
-
-    public void criarUsuario(String nome, String email, String senha, String endereco, String cpf) {
-        system.criarUsuario(nome, email, senha, endereco, cpf);
-    }
-
-    public UUID login(String email, String senha) {
-        return system.login(email, senha);
-    }
-
-    public void encerrarSistema() {
-        system.encerrarSistema();
-    }
 }
