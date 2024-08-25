18611714bbfe88f00306f00e9e0e79bbd121a24e
filipe <filipeflicks@gmail.com>
2024-08-25 14:14:35 -0300

    bootstrap

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
-import easyaccept.EasyAccept;
+import br.ufal.ic.p2.myfood.Facade;
+import easyaccept.EasyAcceptFacade;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String[] args2 = {
-                "MyFood.Facade",
-                "tests/us1_1.txt", "tests/us1_2.txt",
-                "tests/us2_1.txt", "tests/us2_2.txt",
-                "tests/us3_1.txt", "tests/us3_2.txt",
-                "tests/us4_1.txt", "tests/us4_2.txt",
-        };
-        EasyAccept.main(args2);
+        List<String> tests = new ArrayList<>();
+
+        tests.add("tests/us1_1.txt");
+        tests.add("tests/us1_2.txt");
+        tests.add("tests/us2_2.txt");
+        tests.add("tests/us2_2.txt");
+        tests.add("tests/us3_1.txt");
+        tests.add("tests/us3_2.txt");
+        tests.add("tests/us4_1.txt");
+        tests.add("tests/us4_2.txt");
+
+        EasyAcceptFacade eaFacade = new EasyAcceptFacade(new Facade(), tests);
+
+        // Execute the tests
+        eaFacade.executeTests();
+
+        // Print the tests execution results
+        System.out.println(eaFacade.getCompleteResults());
     }
 }
---
+++ src/br/ufal/ic/p2/myfood/Facade.java
@@ -0,0 +1,7 @@
+package br.ufal.ic.p2.myfood;
+
+public class Facade {
+    public void zerarSistema() {
+
+    }
+}
